f2b6d828822892b4e98b4e0360d044d6e174b289
Shailesh Tagadghar <128667791+Shailesh-Tagadghar@users.noreply.github.com>
2023-11-10 10:37:30 +0530

    Update Practical 05  DAG-Generation.py

--- DAG-Generation.py
+++ DAG-Generation.py
@@ -35,3 +35,37 @@ pos = nx.spring_layout(dag)
 nx.draw(dag, with_labels=True,node_size=500,node_color="lightblue", font_size=10)
 plt.title("Expression DAG")
 plt.show()
+
+
+
+////////////////////////////////////////////// Mihir Updated Code ////////////////////////////////////////////////////
+
+
+import networkx as nx
+import matplotlib.pyplot as plt
+from sympy import parse_expr
+
+def create_expression_dag(expression):
+    G = nx.DiGraph()
+    parsed_expr = parse_expr(expression, evaluate=False)
+    
+    def add_nodes_and_edges(expr):
+        if expr.is_Atom:
+            G.add_node(expr)
+        else:
+            for arg in expr.args:
+                G.add_edge(expr, arg)
+                add_nodes_and_edges(arg)
+                
+    add_nodes_and_edges(parsed_expr)
+    
+    return G
+
+expression = "2+3+5*2+2+3"
+dag = create_expression_dag(expression)
+
+pos = nx.spring_layout(dag)
+nx.draw(dag, pos, with_labels=True, node_size=500, node_color='lightblue', font_size=10)
+plt.title("Expression DAG")
+plt.show()
+
