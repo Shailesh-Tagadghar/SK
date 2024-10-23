f481a1d5808463a42c408ff106be9d66c596da06
Shailesh Tagadghar <128667791+Shailesh-Tagadghar@users.noreply.github.com>
2024-10-23 22:25:22 +0530

    Create AuctionC.sol

---
+++ Block Chain/Practical 6/Program 1/AuctionC.sol
@@ -0,0 +1,36 @@
+Program 2 -- write a simple auction contract where user can bid on an items when a highest user bid.
+
+
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract AuctionC {
+    address public o;
+    uint256 public hb;
+    address public hba;
+    bool public oe;
+
+    constructor() {
+        o = msg.sender;
+        hb = 0;
+        oe = false;
+    }
+
+    function bid(uint a) public {
+        require(!oe, "Auction has already ended");
+        require(a > hb, "Bid must be greater then current value");
+        hb = a;
+        hba = msg.sender;
+    }
+
+    function endA() public {
+        require(msg.sender == o, "only owner can end");
+        oe = true;
+    }
+
+    function withdraw() public view returns (uint256) {
+        require(msg.sender == hba, "only highest bidder can withdraw");
+        return hb;
+    }
+}
