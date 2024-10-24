d877e5a93024a18d8ec3a5c8460105aed8572fc4
Shailesh Tagadghar <128667791+Shailesh-Tagadghar@users.noreply.github.com>
2024-10-24 12:20:29 +0530

    Practical 1 -- Addition Two Number , Factorial

--- Block Chain/Practical 1/test.js
+++ Block Chain/Practical 1/test.js
@@ -1,3 +1,4 @@
+//additon of two number
 const { Block, Blockchain } = require('./blockchain');
 let mb = new Blockchain();
 console.log("Shailesh Tagadghar")
@@ -9,5 +10,13 @@ console.log(JSON.stringify(mb, null, 3));
 
 
 
-// to run this code  -- node test.js 
-// if required any module to install -- npm install ejs , npm install crypto-js
+// // to run this code  -- node test.js 
+// // if required any module to install -- npm install ejs , npm install crypto-js
+
+//Factorial
+const { b, bc, Blockchain, Block } = require('./factorial')
+let mb = new Blockchain();
+console.log("Developed by Shailesh Tagadghar - 31031523034")
+mb.addBlock(new Block(1, '01/08/2024', 5))
+mb.addBlock(new Block(2, '01/08/2024', 12))
+console.log(JSON.stringify(mb, null, 3));
